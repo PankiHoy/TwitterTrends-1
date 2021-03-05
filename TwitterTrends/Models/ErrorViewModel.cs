3846ab8522175408ca285706c07bbf0d2681a97a
MiroshnichenkoArtem <57839029+MiroshnichenkoArtem@users.noreply.github.com>
2021-03-05 14:00:32 +0300

    Добавьте файлы проекта.

---
+++ TwitterTrends/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TwitterTrends.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
