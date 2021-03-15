24a13278bd31984092b05b8f76da4027be98130a
PankiHoy <dasssive_mick@mail.ru>
2021-03-15 17:30:32 +0300

    Update site.js

--- TwitterTrends/wwwroot/js/site.js
+++ TwitterTrends/wwwroot/js/site.js
@@ -1,4 +1,42 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var DrawState = new function() {
+    var that = this,
+        nodes,
+        context,
+        points = [];
+   
 
-// Write your JavaScript code.
+    var drawCanvasPoints = function(State){
+        context.fillStyle = State.getColor();
+        context.lineWidth = 1;
+        context.strokeStyle = 'rgb(0,0,0)';
+        context.beginPath();
+        let mas = []; mas = State.getPolygons().toArray;
+        mas.array.forEach(function(mas, i) {
+            if (i == 0) {
+                context.moveTo(mas[0], mas[1]);
+            }
+            else {
+                context.lineTo(mas[0], mas[1]);
+            }
+        });
+        context.closePath();
+        context.fill();
+        context.stroke();
+    }
+
+    that.init = function() {
+        nodes = {
+            'draw' : _.getElement('draw'),
+            'canvas' : _.getElement('canvas')
+        };
+        nodes['canvas'].width = nodes['draw'].offsetWidth;
+        nodes['canvas'].height = nodes['draw'].offsetHeight;
+        context = nodes['canvas'].getContext('2d');
+        clearBtn();
+        renderBtn();
+    }
+
+
+
+
+}
