0f372b7fad6515c96caa6dc2875da57d208e3808
MiroshnichenkoArtem <57839029+MiroshnichenkoArtem@users.noreply.github.com>
2021-03-16 20:56:55 +0300

    Added regex to trie and TweetBuilder

--- DataProcessing/PhrasesMatching/Trie.cs
+++ DataProcessing/PhrasesMatching/Trie.cs
@@ -1,47 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace DataProcessing.PhrasesMatching
 {
-    /// <summary>
-    /// Trie that will find strings or phrases and return values of type <typeparamref name="T"/>
-    /// for each string or phrase found.
-    /// </summary>
-    /// <remarks>
-    /// <typeparamref name="T"/> will typically be a char for finding strings
-    /// or a string for finding phrases or whole words.
-    /// </remarks>
-    /// <typeparam name="T">The type of a letter in a word.</typeparam>
-    /// <typeparam name="TValue">The type of the value that will be returned when the word is found.</typeparam>
+   
     public class Trie
     {
-        private char[] separator = {' ', '.', ',', '#', '\t'};
-        /// <summary>
-        /// Root of the trie. It has no value and no parent.
-        /// </summary>
+       
+        private readonly string pattern = @"[().,;:?\/!@#%'\|]";//FIX THIS PLS TO MATCH WHOLE PUNCTUATION
+        private Regex regex;
+        public Trie()
+        {
+            regex = new Regex(pattern);
+        }
+       
         private readonly Node _root = new Node();
 
-        /// <summary>
-        /// Adds a word to the tree.
-        /// </summary>
-        /// <remarks>
-        /// A word consists of letters. A node is built for each letter.
-        /// If the letter type is char, then the word will be a string, since it consists of letters.
-        /// But a letter could also be a string which means that a node will be added
-        /// for each word and so the word is actually a phrase.
-        /// </remarks>
-        /// <param name="phrase">The word that will be searched.</param>
-        /// <param name="value">The value that will be returned when the word is found.</param>
         public void Add(string phrase)
         {
-            // start at the root
             var node = _root;
 
-            IEnumerable<string> words = phrase.Split();//with separator?
-
-            // build a branch for the word, one letter at a time
-            // if a letter node doesn't exist, add it
+            IEnumerable<string> words = phrase.Split();
             foreach (string word in words)
             {
                 var child = node[word];
@@ -52,16 +33,9 @@ namespace DataProcessing.PhrasesMatching
                 node = child;
             }
 
-            // mark the end of the branch
-            // by adding a value that will be returned when this word is found in a text
             node.IsEndPhrase = true;
         }
 
-        /// <summary>
-        /// Finds all added words in a text.
-        /// </summary>
-        /// <param name="text">The text to search in.</param>
-        /// <returns>The values that were added for the found words.</returns>
         public List<string> Find(string text)
         {
             var node = _root;
@@ -69,7 +43,7 @@ namespace DataProcessing.PhrasesMatching
             List<string> foundPhrases = new List<string>();
             StringBuilder phrase = new StringBuilder();
 
-            string[] words = text.Split(separator);//change to Regex.Split?
+            string[] words = regex.Replace(text, "").Trim().Split();
             for (int i = 0; i < words.Length;)
             {
                 var wordToSearch = words[i].ToLower();
@@ -106,37 +80,21 @@ namespace DataProcessing.PhrasesMatching
             return foundPhrases;
         }
 
-
-        /// <summary>
-        /// Node in a trie.
-        /// </summary>
-        /// <typeparam name="TNode">The same as the parent type.</typeparam>
-        /// <typeparam name="TNodeValue">The same as the parent value type.</typeparam>
         private class Node : IEnumerable<Node>
         {
             private readonly string _word;
             private readonly Dictionary<string, Node> _children = new Dictionary<string, Node>();
             private bool _isEndPhrase = false;
 
-            /// <summary>
-            /// Constructor for the root node.
-            /// </summary>
             public Node()
             {
             }
 
-            /// <summary>
-            /// Constructor for a node with a word
-            /// </summary>
-            /// <param name="word"></param>
             public Node(string word)
             {
                 _word = word;
             }
 
-            /// <summary>
-            /// Word (or letter) for this node.
-            /// </summary>
             public string Word
             {
                 get { return _word; }
@@ -148,11 +106,6 @@ namespace DataProcessing.PhrasesMatching
                 set => _isEndPhrase = value;
             }
 
-            /// <summary>
-            /// Children for this node.
-            /// </summary>
-            /// <param name="c">Child word.</param>
-            /// <returns>Child node.</returns>
             public Node this[string c]
             {
                 get { return _children.ContainsKey(c) ? _children[c] : null; }
@@ -160,19 +113,16 @@ namespace DataProcessing.PhrasesMatching
             }
 
 
-            /// <inherit/>
             public IEnumerator<Node> GetEnumerator()
             {
                 return _children.Values.GetEnumerator();
             }
 
-            /// <inherit/>
             IEnumerator IEnumerable.GetEnumerator()
             {
                 return GetEnumerator();
             }
 
-            /// <inherit/>
             public override string ToString()
             {
                 return Word.ToString();
--- DataProcessing/TweetBuilder.cs
+++ DataProcessing/TweetBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Core;
 
 namespace DataProcessing
@@ -9,18 +10,37 @@ namespace DataProcessing
     {
         private SentimentCounter _sentimentCounter;
         //private StateDifiner _stateDefiner;
-        private TextParser _txtParser;
+        private readonly TextParser _txtParser;
+        private List<Tweet> _tweets;
+        public List<Tweet> Tweets => _tweets;
 
         public TweetBuilder()
         {
+            _tweets = new List<Tweet>();
             _sentimentCounter = new SentimentCounter();
             _txtParser = TextParser.GetInstance();
+
             //_stateDefiner = new StateDefiner();
         }
-        public Tweet BuildTweet()
+        public  void BuildTweet(string tweetLine)
         {
-            return null;
+            Tweet tweet = new Tweet();
+           _txtParser.TweetParse(tweetLine, tweet);
+            _sentimentCounter.CountSentiments(tweet);
+            
+            //_stateDefiner.DefineState(tweet);
+            _tweets.Add(tweet);
+            
         }
+
+        public async void BuildTweets(string path)
+        {
+            foreach (var line in _txtParser.GetFileLines(path))
+            {
+                await Task.Run(() => BuildTweet(line));
+            }
+        }
+
 
     }
 }
