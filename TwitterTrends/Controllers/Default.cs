9eda1f361c83bd76291157faff91c41cbc0daa2f
PankiHoy <dasssive_mick@mail.ru>
2021-03-27 20:01:18 +0300

    made some changes in map positioning and fixed tooltip naming

--- TwitterTrends/Controllers/Default.cs
+++ TwitterTrends/Controllers/Default.cs
@@ -21,9 +21,9 @@ namespace TwitterTrends.Controllers
                 new MapsColorMapping {From = -10000, To = -3000, Color = "rgb(0,28,117)"},
                 new MapsColorMapping {From = -3000, To = -2000, Color = "rgb(50,166,255)"},
                 new MapsColorMapping {From = -2000, To = -1000, Color = "rgb(130,216,245)"},
-                new MapsColorMapping {From = -1000, To = -0.00001, Color = "rgb(0, 192, 166)"},
+                new MapsColorMapping {From = -1000, To = -0.1, Color = "rgb(0, 192, 166)"},
                 new MapsColorMapping {From = 0, To = 0, Color = "rgb(255, 255, 255)"},
-                new MapsColorMapping {From = 0.0001, To = 500, Color = "rgb(250, 224, 5)"},
+                new MapsColorMapping {From = 0.1, To = 500, Color = "rgb(250, 224, 5)"},
                 new MapsColorMapping {From = 500, To = 1500, Color = "rgb(250, 233, 100)"},
                 new MapsColorMapping {From = 1500, To = 2500, Color = "rgb(250, 191, 5)"},
                 new MapsColorMapping {From = 2500, To = 5000, Color = "rgb(250, 98, 5)"}
@@ -31,7 +31,7 @@ namespace TwitterTrends.Controllers
             ViewBag.colorData = dataColor;
 
             return View();
-            }
+        }
 
         public object GetUSMap()
         {
@@ -42,17 +42,9 @@ namespace TwitterTrends.Controllers
         public object GetSentimentData()
         {
             var tweetBuilder = new TweetBuilder();
-            tweetBuilder.BuildTweets(@"D:\programming\TwitterTrends\DataProcessing\DataToProcess\snow_tweets2014.txt");
+            tweetBuilder.BuildTweets(@"/Users/dev/Documents/GitHub/TwitterTrends/DataProcessing/DataToProcess/cali_tweets2014.txt");
 
             return tweetBuilder.StatesForDisplay;
         }
-
-        public class MapDataSetting
-        {
-            public Boolean async { get; set; }
-            public String dataOptions { get; set; }
-            public String type { get; set; }
-        }
-
     }
 }
