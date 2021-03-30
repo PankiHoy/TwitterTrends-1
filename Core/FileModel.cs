01a765cf5768b306858e5af33424246823427501
MiroshnichenkoArtem <57839029+MiroshnichenkoArtem@users.noreply.github.com>
2021-03-30 18:29:15 +0300

    Added form to choose maps to output

--- Core/FileModel.cs
+++ Core/FileModel.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Core
 {
     public class FileModel
     {
+       
         public int Id { get; set; }
         public string Name { get; set; }
+        [Key]
         public string Path { get; set; }
     }
 }
